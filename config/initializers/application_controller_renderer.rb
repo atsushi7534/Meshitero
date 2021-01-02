f460a3fd19b96f9937bfb3ae8bee7a095c44ae36
atsushi7534 <atsushi1007@outlook.jp>
2021-01-02 08:51:37 +0000

    Finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd3c611904f20f490aa8d05cd641d9f474deb55c9e17c1f1be40c62ca08ff5f7badf1628fc4e0e18acb1e23a63f2b1ff464db4fb9615a4d1733d217eaa6a9dae8'
